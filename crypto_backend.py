86ca6ea66fe693d9f2de49e41aa0b28d4ecb62fd
NaHid <nahid.xsr@gmail.com>
2025-02-11 11:15:34 +0600

    Update crypto_backend.py

--- crypto_backend.py
+++ crypto_backend.py
@@ -1,1 +1,17 @@
+from flask import Flask, request, jsonify
 
+app = Flask(__name__)
+
+@app.route("/")
+def home():
+    return "Crypto Wallet Backend Running!"
+
+@app.route("/deposit", methods=["POST"])
+def deposit():
+    data = request.json
+    user_address = data.get("user_address")
+    amount = data.get("amount")
+    return jsonify({"message": "Deposit successful!", "user": user_address, "amount": amount})
+
+if __name__ == "__main__":
+    app.run(debug=True)
