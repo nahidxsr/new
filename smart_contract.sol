9a06d277bbf48e52c739f7fbac81f9891b7d530b
NaHid <nahid.xsr@gmail.com>
2025-02-11 11:15:02 +0600

    Update smart_contract.sol

--- smart_contract.sol
+++ smart_contract.sol
@@ -1,1 +1,21 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
 
+contract CryptoDeposit {
+    mapping(address => uint256) public balances;
+
+    function deposit() public payable {
+        require(msg.value > 0, "Must send some ETH");
+        balances[msg.sender] += msg.value;
+    }
+
+    function withdraw(uint256 amount) public {
+        require(balances[msg.sender] >= amount, "Insufficient balance");
+        payable(msg.sender).transfer(amount);
+        balances[msg.sender] -= amount;
+    }
+
+    function getBalance() public view returns (uint256) {
+        return balances[msg.sender];
+    }
+}
